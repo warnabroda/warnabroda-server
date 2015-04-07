e61439d68a06a891ec22728df31aa58c792b8369
Herbert Silva <hbt.vieira@gmail.com>
2015-04-07 10:16:34 -0300

    ajuste nome database

--- models/gorp.go
+++ models/gorp.go
@@ -105,7 +105,7 @@ const (
 func init() {
 	log.Println("Opening db...")
 	var password = os.Getenv("WARNAPASS")
-	path := []string{"root:", password, "@(localhost:3306)/warnabroda_warn_return"}
+	path := []string{"root:", password, "@(localhost:3306)/warnabroda"}
 
 	db, err := sql.Open("mysql", strings.Join(path, ""))
 	checkErr(err, "opening db failed")
